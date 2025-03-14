3408e801975017aba765ec2d049249c8dbc0b960
MdSaied2025 <md.saied2023@gmail.com>
2025-03-14 11:40:29 +0300

    Q3,4

---
+++ session2/q3.dart
@@ -0,0 +1,23 @@
+/* Set Properties
+Objective: Learn about sets by checking properties like size and membership.
+Instructions:
+- Create a set named uniqueNumbers with the values {10, 20, 30, 40}.
+- Print the length of uniqueNumbers.
+- Check if the number 20 is present in the set and print the result. */
+
+void main() {
+  Set<int> uniqueNumbers = {10, 20, 30, 40};
+  print(uniqueNumbers.length);
+  int UserNumber = 20;
+  if (uniqueNumbers.contains(UserNumber)) {
+    print('the value : ' +
+        UserNumber.toString() +
+        ' ' +
+        'is exist in a set uniqueNumbers');
+  } else {
+    print('the value : ' +
+        UserNumber.toString() +
+        ' ' +
+        'is Not exist in a set uniqueNumbers');
+  }
+}
