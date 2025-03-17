cf7ee156771fc917d763f8e033dd9248b81d8ede
MdSaied2025 <md.saied2023@gmail.com>
2025-03-17 12:47:04 +0300

    q5

--- session3/q5.dart
+++ session3/q5.dart
@@ -1,3 +1,25 @@
 /* 5. Class Creation and Dot Operator Usage:
 Create a class with a few properties and methods. Instantiate the class and use the dot operator to
 access and print its properties and methods. */
+
+
+class Animal {
+  String? name;
+  int? numberOfLegs;
+  int? lifeSpan;
+
+  void display() {
+    print("Animal name: $name.");
+    print("Number of Legs: $numberOfLegs.");
+    print("Life Span: $lifeSpan.");
+  }
+}
+
+void main(){
+  // Here animal is object of class Animal. 
+  Animal animal = Animal();
+  animal.name = "Lion";
+  animal.numberOfLegs = 4;
+  animal.lifeSpan = 10;
+  animal.display();
+}
