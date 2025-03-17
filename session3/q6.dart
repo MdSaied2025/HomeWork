f713971b33df9109c5b318c09587b1a9760a85df
MdSaied2025 <md.saied2023@gmail.com>
2025-03-17 12:40:58 +0300

    q6

--- session3/q6.dart
+++ session3/q6.dart
@@ -2,3 +2,26 @@
 - Task 1: Create a map representing a student with keys for name, age, and grade. Add, update,
 and remove entries from the map, printing the map after each operation.
 - Task 2: Iterate over the map and print each key-value pair. */
+void main(){
+
+var mystudent = new Map();
+{
+mystudent["name"] ="ahmed";
+mystudent["age"] = 16;
+mystudent["grade"] =63;
+
+//print(mystudent.values);
+mystudent.forEach((key,value){
+print(value);
+mystudent.addAll(1,"ali");
+  
+  
+  
+ mystudent.remove(1); 
+  
+  }
+
+  
+//mystudent.addAll()
+
+}
