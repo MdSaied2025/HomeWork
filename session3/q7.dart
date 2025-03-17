ff11db03d1f61f106051fb099d08afc49d714c2c
MdSaied2025 <md.saied2023@gmail.com>
2025-03-17 11:54:10 +0300

    q7

--- session3/q7.dart
+++ session3/q7.dart
@@ -1,3 +1,17 @@
 /* 7. Number Check Program:
 Create a program that asks the user to input a number and checks if it's positive, negative, or zero
 using if-else statements. */
+import "dart:io";
+
+void main() {
+  print('Enter the Number to check its type');
+  int userinputnumber = int.parse(stdin.readLineSync()!);
+
+  if (userinputnumber > 0) {
+    print('the number $userinputnumber is : Positve Number');
+  } else if (userinputnumber < 0) {
+    print('the number $userinputnumber is : Negative Number');
+  } else {
+    print('the number is : Zero');
+  }
+}
