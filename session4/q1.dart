3df591a9018e1e57e667ca0f40cc81c77a141187
MdSaied2025 <md.saied2023@gmail.com>
2025-03-18 18:22:59 +0300

    q1

--- session4/q1.dart
+++ session4/q1.dart
@@ -6,3 +6,18 @@ Create a Dart program that declares and initializes the following variables:
 - Whether you are a student (bool)
 Then, print these values using a single print statement.
  */
+
+void main() {
+  {
+    var Result = {getPesonInfoDetails()};
+  }
+}
+
+void getPesonInfoDetails() {
+  String name = 'Sheren';
+  int yourAge = 36;
+  double YourHeight = 172.5;
+  bool is_Student = true;
+
+  print("your name are : $name, Your Age : $yourAge , your height : $YourHeight , Student Status : $is_Student");
+}
